c85bf984680380aa6b22bdada7954ca964e29fea
CLoboguerrero <c.loboguerrero@outlook.com>
2023-07-20 19:16:39 -0600

    Subida Repo

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function BinarioADecimal(num) {
+   let contador = 0;
+   let binNum = num.split('').reverse();
+
+   for (let i = 0; i < binNum.length; i++){
+      let potencia = Math.pow(2, i) * binNum[i];
+      contador += potencia;
+   }
+   return contador;
+   // return parseInt(num, 2) <------ Solución con método.
+}
+
+function DecimalABinario(num) {
+   let arr = [];
+
+   while (num >= 1){
+      arr.push(Math.floor(num % 2));
+      num = num / 2;
+   }
+   return arr.reverse().join('');
+   // return num.tostring(2) <------ Solución con método.
+}
+
+module.exports = {
+   BinarioADecimal,
+   DecimalABinario,
+};
